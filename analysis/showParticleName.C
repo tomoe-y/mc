ade83206625d8b75f59f65ae35da4b0c9e8e6626
hmdyt <konnyaku108@gmail.com>
2021-11-23 19:34:31 +0900

    読み込んだルートファイルを出力するようにした

--- analysis/showParticleName.C
+++ analysis/showParticleName.C
@@ -2,16 +2,27 @@ void showParticleName(
     TString file_path = "/home/tomoe/mc/bench/test0.root"
 ){
     
+    // read root files
     TChain* chain = new TChain("tree");
     chain->Add(file_path);
 
-    TTreeReader reader(chain);
+    // output read files
+    TObjArray *fileElements = chain->GetListOfFiles();
+    TIter next(fileElements);
+    TChainElement *chEl = 0;
+    while (( chEl=(TChainElement*)next() )) {
+        fprintf(stdout, "read\t'%s'\n", chEl->GetTitle());
+    }
 
+    // init TTreeReader instead of SetBranchAddress
+    TTreeReader reader(chain);
     TTreeReaderValue<int> nHit(reader, "nHit");
     TTreeReaderArray<int> particleID(reader, "particle");
 
+    // m_particles = {pdgcode: number of particles}
     map<int, int> n_particles;
 
+    // count number of particles
     while(reader.Next()){
         if (*nHit == 0) continue;
         set<int> particleID_unique(particleID.begin(), particleID.end());
@@ -24,10 +35,11 @@ void showParticleName(
         }
     }
 
+    // output count results while convert PDGcode into particle name
     TDatabasePDG* pdg = new TDatabasePDG();
     for (auto itr = n_particles.begin(); itr != n_particles.end(); ++itr){
         if (itr->first > 1000000000) continue;
-        cout << pdg->GetParticle(itr->first)->GetName() << ": " << itr->second << endl;
+        cout << pdg->GetParticle(itr->first)->GetName() << "\t" << itr->second << endl;
     }
 
 }
